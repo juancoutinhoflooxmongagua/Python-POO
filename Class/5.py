a2c93026dc757e3c386113c101cee6bd7f68194d
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-06-09 09:56:30 -0400

    feat: atribute classes

---
+++ Class/5.py
@@ -0,0 +1,14 @@
+
+
+class Person: 
+    ano_atual = 2025
+
+    def __init__(self, age, name):
+        self.name = name
+        self.age = age
+
+    def get_ano_nascimento(self):
+        return self.ano_atual - self.age
+
+p1 = Person(18, 'Juan')
+print(p1.get_ano_nascimento())

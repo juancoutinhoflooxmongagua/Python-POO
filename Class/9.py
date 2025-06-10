eb492c1833b5632b064ceb8ea09d37ca0a2b85f1
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-06-10 11:10:58 -0400

    feat: @property + @setter - getter e seter in Python

--- Class/9.py
+++ Class/9.py
@@ -1,9 +1,30 @@
+
 class Caneta:
-    def __init__(self, cor):  
-        self._cor = cor 
+    def __init__(self, cor):
+        self.cor = cor
+        self._cor_tampa = None
+
     @property
     def cor(self):
-        return self._cor  
+        print('ESTOU NO GETTER')
+        return self._cor
+
+    @cor.setter
+    def cor(self, valor):
+        print('ESTOU NO SETTER')
+        self._cor = valor
+
+    @property
+    def cor_tampa(self):
+        return self._cor_tampa
+
+    @cor_tampa.setter
+    def cor_tampa(self, valor):
+        self._cor_tampa = valor
+
 
 caneta = Caneta('Azul')
-print(caneta.cor) 
+caneta.cor = input('Digite a cor que vc quer')
+caneta.cor_tampa = 'Azul'
+print(caneta.cor)
+print(caneta.cor_tampa)

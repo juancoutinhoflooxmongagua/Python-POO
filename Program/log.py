359dd88063c3a6954b3e8989a8c80857a4358606
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-06-10 12:36:11 -0400

    feat: part two  module mixins

--- Program/log.py
+++ Program/log.py
@@ -1,1 +1,30 @@
-from log import Log
+class Log:
+    def log(self, msg):
+        return self._log(msg)
+
+    def _log(self, msg):
+        raise NotImplementedError('Implemente o método _log')
+
+    def log_error(self, msg):
+        return self._log(f'Error: {msg}')
+
+    def log_success(self, msg):
+        return self._log(f'Success: {msg}')
+
+
+class LogFileMixin(Log):
+    def _log(self, msg):
+        print(msg)
+
+
+class LogPrintMixin(Log):
+    def _log(self, msg):
+        print(f'{msg} ({self.__class__.__name__})')
+
+
+if __name__ == '__main__':
+    l = LogFileMixin()
+    l.log('qualquer coisa')
+    l = LogPrintMixin()
+    l.log_error('qualquer coisa')
+    l.log_success('Que legal')
--- Program/main.py
+++ Program/main.py
@@ -1,13 +1,1 @@
-class Log:
-    def log(self, msg):
-        raise NotImplementedError('Implemente o método log')
-
-
-class LogFileMixin(Log):
-    def log(self, msg):
-        print(msg)
-
-
-if __name__ == '__main__':
-    l = LogFileMixin()
-    l.log('qualquer coisa')
+from log import Log

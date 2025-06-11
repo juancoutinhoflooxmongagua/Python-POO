22e837b1df0fae377ff1f3f81f230894803a3447
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-06-11 09:17:42 -0400

    feat: __dunder__ methods

---
+++ 10.py
@@ -0,0 +1,13 @@
+#__lt__(self,other) - self < other
+# __le__(self,other) - self <= other
+# __gt__(self,other) - self > other
+# __ge__(self,other) - self >= other
+# __eq__(self,other) - self == other
+# __ne__(self,other) - self != other
+# __add__(self,other) - self + other
+# __sub__(self,other) - self - other
+# __mul__(self,other) - self * other
+# __truediv__(self,other) - self / other
+# __neg__(self) - -self
+# __str__(self) - str
+# __repr__(self) - str

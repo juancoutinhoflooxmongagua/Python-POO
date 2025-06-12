d8ed47b1e60fce6a73ca02448ee85901d06a4fe4
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-06-12 10:49:49 -0400

    feat: init challenge counts of bank

---
+++ Contas/contas.py
@@ -0,0 +1,19 @@
+import abc
+
+
+class Conta(abc.ABC):
+    def __init__(self, agencia, conta, saldo):
+        self.agencia = agencia
+        self.conta = conta
+        self.saldo = saldo
+
+    @abc.abstractclassmethod
+    def sacar(self, valor): ...
+    self.detalhes(f'Saque {valor}')
+    def depositar(self, valor):
+        self.saldo += valor
+        self.detalhes(f'DEPÓSITO {valor}')
+    def detalhes(self, msg=''):
+        print(f'o seu saldo é {self.saldo:.2f} {msg}')
+
+    

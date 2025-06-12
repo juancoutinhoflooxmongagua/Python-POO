cf48b7e3b66c0566a302663b396677b69dbdac98
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-06-12 10:58:19 -0400

    feat: create Class Conta Corrente

--- Contas/contas.py
+++ Contas/contas.py
@@ -7,29 +7,56 @@ class Conta(abc.ABC):
         self.conta = conta
         self.saldo = saldo
 
-    @abc.abstractclassmethod
+    @abc.abstractmethod
     def sacar(self, valor): ...
-    self.detalhes(f'Saque {valor}')
+
     def depositar(self, valor):
         self.saldo += valor
         self.detalhes(f'DEPÓSITO {valor}')
+
     def detalhes(self, msg=''):
         print(f'o seu saldo é {self.saldo:.2f} {msg}')
 
-    class ContaPoupanca(Conta):
-        def sacar(self, valor):
-            valor_pos_saque = self.saldo - valor
 
-            if valor_pos_saque >= 0:
-              self.saldo = valor 
-              self.detalhes(f'SAQUE {valor}')
-              return self.saldo
+class ContaPoupanca(Conta):
+    def sacar(self, valor):
+        valor_pos_saque = self.saldo - valor
 
-            print('Não foi possivel sacar o valor desejado')
+        if valor_pos_saque >= 0:
+            self.saldo -= valor  # Correção aqui: self.saldo -= valor
+            self.detalhes(f'SAQUE {valor}')
+            return self.saldo
 
-    if __name__ == '__main__':
-        cp1 = ContaPoupanca(111, 222, 0)
-        cp1.sacar(1)
-        cp1.depositar(1)
-        cp1.sacar(1)
-    
+        print('Não foi possivel sacar o valor desejado')
+
+
+class ContaCorrente(Conta):
+    def __init__(self, agencia, conta, saldo, limite):
+        super().__init__(agencia, conta, saldo)
+        self.limite = limite
+
+    def sacar(self, valor):
+        valor_pos_saque = self.saldo - valor
+        limite_maximo_saque = -self.limite
+
+        if valor_pos_saque >= limite_maximo_saque:
+            self.saldo -= valor
+            self.detalhes(f'SAQUE {valor}')
+            return self.saldo
+        
+        print('Não foi possível sacar o valor desejado. Limite excedido.')
+
+
+if __name__ == '__main__':
+    cp1 = ContaPoupanca(111, 222, 0)
+    cp1.sacar(1)
+    cp1.depositar(100)
+    cp1.sacar(1)
+    print('---')
+
+    cc1 = ContaCorrente(111, 333, 0, 100)
+    cc1.sacar(1)
+    cc1.depositar(100)
+    cc1.sacar(150)
+    cc1.sacar(50)
+    cc1.sacar(1)

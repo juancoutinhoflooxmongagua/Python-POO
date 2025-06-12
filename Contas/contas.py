7ab4ae192d666ca961c5002209f3916f1db07b47
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-06-12 10:54:07 -0400

    feat: create Class Poupanca

--- Contas/contas.py
+++ Contas/contas.py
@@ -16,4 +16,20 @@ class Conta(abc.ABC):
     def detalhes(self, msg=''):
         print(f'o seu saldo é {self.saldo:.2f} {msg}')
 
+    class ContaPoupanca(Conta):
+        def sacar(self, valor):
+            valor_pos_saque = self.saldo - valor
+
+            if valor_pos_saque >= 0:
+              self.saldo = valor 
+              self.detalhes(f'SAQUE {valor}')
+              return self.saldo
+
+            print('Não foi possivel sacar o valor desejado')
+
+    if __name__ == '__main__':
+        cp1 = ContaPoupanca(111, 222, 0)
+        cp1.sacar(1)
+        cp1.depositar(1)
+        cp1.sacar(1)
     

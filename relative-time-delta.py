af1cf858b8f27c3054aa67fdc340fd812e40e34c
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-06-13 10:38:24 -0400

    feat: relatime time Delta

---
+++ relative-time-delta.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+from dateutil.relativedelta import relativedelta
+
+fmt = "%d/%m/%Y %H:%M:%S"
+data_inicio = datetime.strptime("20/04/1987 09:30:30", fmt)
+data_fim = datetime.strptime("12/12/2022 08:20:20", fmt)
+# delta = timedelta(days=10, hours=2)Add commentMore actions
+delta = relativedelta(data_fim, data_inicio)
+print(delta.days, delta.years)

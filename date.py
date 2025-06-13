d82ce277844e43c984f6d42c4d17b8b46e7a147f
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-06-13 09:57:24 -0400

    feat: date and hour in python

---
+++ date.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+data_str_data = "2022/04/20 07:49:23"
+data_str_data = "20/04/2022"
+data_str_fmt = "%d/%m/%Y"
+
+# data = datetime(2022, 4, 20, 7, 49, 23)
+data = datetime.strptime(data_str_data, data_str_fmt)
+print(data)

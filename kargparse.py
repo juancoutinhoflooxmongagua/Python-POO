88ba77f5e2cc42a7582062af65bb2a86cd501d4c
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-06-16 12:22:39 -0300

    feat: addnew args

--- kargparse.py
+++ kargparse.py
@@ -2,11 +2,24 @@ from argparse import ArgumentParser
 
 parser = ArgumentParser()
 
-parser.add_argument("-b", "--basic")
-
+parser.add_argument(
+    "-b",
+    "--basic",
+    help='Mostra "Olá mundo" na tela',
+    # type=str, # Tipo do argumento
+    metavar="STRING",
+    # default='Olá mundo', # Valor padrão
+    required=False,
+    action="append",  # Recebe o argumento mais de uma vez
+    # nargs='+', # Recebe mais de um valor
+)
+parser.add_argument("-v", "--verbose", help="Mostra logs", action="store_true")
 args = parser.parse_args()
 
 if args.basic is None:
-    print("vc n passou o valor de b")
+    print("Você não passou o valor de b.")
+    print(args.basic)
 else:
-    print("o valor de b:", args.basic)
+    print("O valor de basic:", args.basic)
+
+print(args.verbose)

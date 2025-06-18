d8306318d6209abf06540652c60324b8f824795f
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-06-18 10:39:04 -0300

    feat: openpyxl Workbook and Worksheet

---
+++ kopenpyxl.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+from openpyxl import Workbook
+from openpyxl.worksheet.worksheet import Worksheet
+
+ROOT_FOLDER = Path(__file__).parent
+WORKBOOK_PATH = ROOT_FOLDER / 'workbook.xlsx'
+
+workbook = Workbook()
+worksheet: Worksheet = workbook.active
+
+# Criando os cabeçalhos
+worksheet.cell(1, 1, 'Nome')
+worksheet.cell(1, 2, 'Idade')
+worksheet.cell(1, 3, 'Nota')
+
+students = [
+    # nome      idade nota
+    ['João',    14,   5.5],
+    ['Maria',   13,   9.7],
+    ['Luiz',    15,   8.8],
+    ['Alberto', 16,   10],
+]
+
+for student in students:Add commentMore actions
+    worksheet.append(student)
+
+workbook.save(WORKBOOK_PATH)
